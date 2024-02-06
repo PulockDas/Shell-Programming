e504ccc25d4497429793b567284915ca2a0ca36b
pulockdas <pulockkamol50@gmail.com>
2024-02-06 19:44:53 +0600

    Learned about shell variables and added example code.

---
+++ Variables/basic_var.sh
@@ -0,0 +1,28 @@
+# making variables readonly
+readonly AGE=25
+echo $AGE
+
+AGE=30 # WARNINHG## AGE: readonly variable
+echo $AGE
+
+
+
+# use of unset in variables
+AGE2=30
+unset AGE2
+echo empty:$AGE2 # only empty will be printed
+
+
+
+# use of Global variable in a function
+setAge () {
+    echo "Inside function AGE: $AGE"
+}
+setAge # "Inside function AGE: 25" will be printed. AGE is a global var here.
+
+
+
+
+# declare variable types in a terminal
+declare -a arr=(value value2)
+echo "array: ${arr[*]}"
